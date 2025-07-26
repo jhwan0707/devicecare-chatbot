5fbf0239e56ec9b433a5182a79637c894237233b
junghwan <90751590+jhwan0707@users.noreply.github.com>
2025-07-26 12:42:58 +0800

    docs: add reset instructions

---
+++ src/components/SupportLink.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SupportLink() {
+  return (
+    <div className="SupportLink">
+      Need more help?{' '}
+      <a href="mailto:support@devicecare.com">Contact Support</a>
+    </div>
+  );
+}
+
+export default SupportLink;
